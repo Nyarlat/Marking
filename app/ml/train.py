585bf4716b4802c7dfdf834e43672bab9eda4828
Nyarlat <nyarlat7@gmail.com>
2024-11-09 23:50:52 +0500

    refactoring

--- app/ml/train.py
+++ app/ml/train.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 if __name__ == '__main__':
-    model = YOLO('yolo11n.pt')
+    model = YOLO('../../yolo11n.pt')
 
     results = model.train(
         data='marking.yaml',
