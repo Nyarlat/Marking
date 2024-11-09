70077c0b082f2e7ef19457b2eb8d91c4abd411f2
Nyarlat <nyarlat7@gmail.com>
2024-11-09 11:21:22 +0500

    add tesseract

--- ocr2.py
+++ ocr2.py
@@ -1,14 +1,11 @@
 import cv2
-import easyocr
+import pytesseract
 from ultralytics import YOLO
 
 # Load the YOLO model
 model = YOLO('best.pt')
 
-# Initialize EasyOCR reader
-reader = easyocr.Reader(['en'])  # Specify the language(s) you want to use
-
-def detect_and_read(image_path):
+def detect_and_read2(image_path):
     # Load the image
     img = cv2.imread(image_path)
 
@@ -22,17 +19,16 @@ def detect_and_read(image_path):
             x1, y1, x2, y2 = map(int, bbox)  # Convert to integer
             # Crop the image to the bounding box area
             cropped_img = img[y1:y2, x1:x2]
-
-            # Use EasyOCR to read text from the cropped image
-            text_results = reader.readtext(cropped_img)
+            pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+            # Use Tesseract to read text from the cropped image
+            text = pytesseract.image_to_string(cropped_img)
 
             # Print detected text
-            for (bbox, text, prob) in text_results:
+            if text.strip():  # Check if text is not empty
                 full_text += text + " "
-                # print(f"Detected text: {text} with confidence: {prob:.2f}")
 
     # print(full_text.strip())
     return full_text.strip()
 
 if __name__ == '__main__':
-    detect_and_read('4.JPG')
+    detect_and_read2('4.JPG')
