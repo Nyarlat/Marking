dd01d866a1dfa236026171d2bfa47787116e6020
Kashanaft <54658329+RuslanKashapov@users.noreply.github.com>
2024-11-10 12:27:38 +0500

    Create test.md

---
+++ test.m
@@ -0,0 +1,10 @@
+# Структура проекта
+- **app/dao/** - логика работы с базой данных
+- **app/data/** - база данных sqlite
+- **app/ml/models/** - веса модели Yolo
+- **app/ml/ocr.py** - обработка изображения Yolo и ocr
+- **app/routers/** - API
+- **app/schemas/** - схема ответа сервера
+- **app/services/** - сравнение с ngram
+
+
